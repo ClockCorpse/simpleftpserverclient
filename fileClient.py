898e5c48c9d32a6effbeb6075b7f14cd7edd08e9
Anh Thien Loi <36415837+ClockCorpse@users.noreply.github.com>
2020-05-13 12:50:51 +0700

    Update fileClient.py
    added upload and login

--- fileClient.py
+++ fileClient.py
@@ -1,5 +1,98 @@
 import socket
+import json
+from getpass import getpass
 import os
+
+def getUserCred():
+    name = input('Enter user name: ')
+    password = getpass('Enter user password: ')
+
+    credential = {'name':name, 'password':password}
+
+    credentialJson = json.dumps(credential)
+    return credentialJson
+
+def login(s,cred):
+    s.send(bytes(cred,'UTF-8'))
+    data = s.recv(1024)
+    data = data.decode('UTF-8')
+    if data == 'Valid':
+        return True
+    if data == 'Invalid':
+        return False
+
+def printMenu():
+    print('0. Exit the program.')
+    print('1. Download a file')
+    print('2. Upload a file')
+
+def chooseMenu():
+    option = 0
+    while True:
+        option = int(input('Choose an option: '))
+        if 0 <= option and option <= 2:
+            break
+    return option
+
+def action(sock):
+    while True:
+        printMenu()
+        option = chooseMenu()
+        if option == 0:
+            sock.close()
+            break
+        elif option == 1:
+            retr(sock) 
+        elif option == 2:
+            store(sock)
+
+
+def retr(sock):
+    fileName = input('Enter file name ->')
+    if fileName !='q':
+        sock.send(bytes('get '+fileName,'UTF-8'))
+        data = sock.recv(1024).decode('UTF-8')
+        if data[:6] == 'EXISTS':
+            fileSize = data[6:]
+            message = input('File exists '+fileSize+' B, download?(Y/N)-> ')
+            if message == 'Y':
+                sock.send(bytes('OK','UTF-8'))
+                with open(fileName,'wb') as f:
+                    data = sock.recv(1024)
+                    f.write(data)
+                    totalRecv = len(data)
+                    while totalRecv < int(fileSize):
+                        data = sock.recv(1024)
+                        f.write(data)
+                        totalRecv +=len(data)
+                    print('Download Complete!')
+                f.close()
+        else:
+            print('File does not exists!')
+
+def store(sock):
+    fileName = input('Enter file name ->')
+    curDir = os.path.dirname(os.path.abspath(__file__))
+    fileLocation = curDir+'/'+fileName
+    if fileName != 'q':
+        if os.path.isfile(fileLocation):
+            sock.send(bytes('put '+fileName+','+str(os.path.getsize(fileLocation)),'UTF-8'))
+            response = sock.recv(1024).decode('UTF-8')
+            if response[:2] == 'OK':
+                print('Uploading')
+                with open(fileName,'rb') as f:
+                    bytesToSend = f.read(1024)
+                    sock.send(bytesToSend)
+                    while bytesToSend != b'':
+                        bytesToSend = f.read(1024)
+                        sock.send(bytesToSend)
+                print('Upload complete!')
+                f.close()
+        else:
+            print('File does not exists!')
+
+
+
 
 def Main():
     host = input("Enter server's IP address: ")
@@ -8,30 +101,12 @@ def Main():
     s = socket.socket()
     s.connect((host,port))
 
-    fileName = input("Enter filename ->")
-    if fileName != 'q':
-        s.send(bytes(fileName,'UTF-8'))
-        data = (s.recv(1024))
-        data = data.decode('UTF-8')
-        if data[:6]=="EXISTS":
-            fileSize = data[6:]
-            message = input('File exists,'+fileSize+' B, download? (Y/N)-> ')
-            if message == 'Y':
-                s.send(bytes('OK','UTF-8'))
-                newFileName = 'new_'+fileName
-                f = open(newFileName, 'wb')
-                data = s.recv(1024)
-                totalReceive = len(data)
-                f.write(data)
-                while totalReceive < int(fileSize):
-                    data = s.recv(1024)
-                    f.write(data)
-                    totalReceive +=len(data)
-                    print ("{0:.2f}".format((totalReceive/int(fileSize))*100)+ "% Done")
-                print("Download Complete!")
-                f.close()
-        else:
-            print("File does not exists!")
+    cred = getUserCred()
+    if login(s,cred) == True:
+        print('Login success!')
+        action(s)
+    else:
+        print('Invalid username or password!')
     s.close()
 
 if __name__ == "__main__":
